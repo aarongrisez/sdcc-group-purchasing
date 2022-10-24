71b7bd25b2f7a90ccbb108d4e3ce6fda4ee1ce23
Aaron Grisez <aaron@aarongrisez.com>
2022-10-23 19:41:00 -0700

    Fixes library import path

--- src/world/main.go
+++ src/world/main.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aarongrisez/sdcc-group-purchasing/lib"
+	"github.com/aarongrisez/sdcc-group-purchasing/src/lib"
 )
 
 // Response is of type APIGatewayProxyResponse since we're leveraging the
